a3f1f5a8f08137c8f8bbec79eb3c1b654db11c83
Ammad Shaikh <ammadshaikh8801@gmail.com>
2023-04-15 11:16:41 -0700

    FLEAT: DAY 01 assignment updated

---
+++ script/app.js
@@ -0,0 +1,5 @@
+/// DAY 01 14 April ////////
+var Amount = +prompt('Enter your  amount', 'enter the amonts in numbers');
+var zakat = 0.025;
+var calculations = zakat*Amount;
+alert("Wajib Zakat on you = " + calculations)
