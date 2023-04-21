156e3589f796336b42c974029b1600e386afe9d6
Ammad Shaikh <ammadshaikh8801@gmail.com>
2023-04-21 04:42:29 -0700

    Fleat: Assignment 01 Completed

---
+++ js Assignment/index.js
@@ -0,0 +1,112 @@
+// task 01
+/*var zakatPercentage = 0.025;
+var userInput = +prompt('Enter the Amount','write in numbers')
+var result = zakatPercentage * userInput;
+alert("Your zakat value is " + result )
+
+// task 02 
+var Gandum = 250;
+var Jo = 450;
+var khajoor = 1000;
+var kishmish = 1800;
+var familyMembers = +prompt('Please enter the total number of family members','write in numbers');
+var  fitraMethod =  +prompt('There are four method of fitra (1)Gandum ,(2)jo , (3)khajoor and (4)kishmish', 'please select in number 1 to 4');
+
+if(fitraMethod == 1){
+  alert("Wajib fitra on your family  " + Gandum * familyMembers);
+}
+else if(fitraMethod == 2){
+    alert("Wajib fitra on your family  " + Jo * familyMembers);
+}
+else if(fitraMethod == 3){  
+    alert("Wajib fitra on your family  " + khajoor * familyMembers);
+}
+else if(fitraMethod == 4){
+    alert("Wajib fitra on your family " + kishmish * familyMembers);
+}
+else{
+    alert("Sorry your information is incomplete")
+};*/
+
+//        task 03
+ /* var secretNumber = 5;
+var guessNumber = +prompt("please guess a number between 1 t0 10");
+
+if(guessNumber === secretNumber){
+    alert("Congratulations!")
+}
+else if(guessNumber > secretNumber && guessNumber <= 10){
+    alert("guess is too high ")
+}
+else if(guessNumber < secretNumber ){
+    alert("guess is too low ")
+} 
+else{
+    alert("your guess is out of the range")
+} */
+
+//  Task 04
+
+/*  var yourName = prompt("Enter your Name")
+var abc = yourName.toUpperCase();
+alert(abc) */
+
+/// Task 05
+
+/* var contactNumber = [];
+var contactName = [];
+var AddNumber = +prompt("please Add number")
+var AddName = prompt("please Add Names")
+
+contactNumber.push(AddNumber)
+contactName.push(AddName)
+console.log("the Contact and number is" + contactNumber + "Contact Name is" + contactName) */
+
+// Task 06
+
+/* var products = ["Mobile", "Laptop", "Computer", "Tablet", "Macbook"];
+var position = prompt("Enter the position");
+
+var remove = products.splice(position,3);
+
+alert(remove); */
+
+ // Task 07
+
+/*  var nationality = prompt("What is your nationality?");
+var  gender = prompt("What is your gender?");
+var  age = +prompt("What is your age?");
+
+if( nationality.toLowerCase() === "pakistani" || nationality.toLowerCase() === "indian") {
+
+if (gender.toLowerCase() === "male" && age >= 18) {
+    alert("You are eligible to vote!");
+  }
+    else if (gender.toLowerCase() === "female" && age>= 18) {
+    
+    var Marriedlady = prompt("Are you married? yes or no" );
+
+    if (Marriedlady.toLowerCase() === "yes") {
+      alert("You are eligible to vote!");
+    } 
+    else {
+      alert("You are not eligible to vote.");
+    }
+  }
+   else {
+    alert("You are not eligible to vote.");
+  }
+
+} 
+else {
+  alert("You are not eligible to vote.");
+} 
+ */
+// Task 08
+/* 
+var WorldCupSquad = ["player1","player2","player3","player4","player5","player6","player7","player8","player9","player10","player11","player12","player13","player14","player15"]
+
+var playingEleven = WorldCupSquad.splice(2,13);
+console.log(playingEleven) */
+
+
